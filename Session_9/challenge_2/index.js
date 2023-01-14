276fad093001b14466adebc5a101acf2c40b7f21
Duy <duytv.dev@gmail.com>
2023-01-14 15:06:32 +0700

    odd victoty

--- Session_9/challenge_2/index.js
+++ Session_9/challenge_2/index.js
@@ -1,56 +1,65 @@
 const game = {
-    team1: "Bayern Munich",
-    team2: "Borrussia Dortmund",
-    players: [
-      [
-        "Neuer",
-        "Pavard",
-        "Martinez",
-        "Alaba",
-        "Davies",
-        "Kimmich",
-        "Goretzk",
-        "Coman",
-        "Muller",
-        "Gnarby",
-        "Lewandowski",
-      ],
-      [
-        "Burki",
-        "Schulz",
-        "Hummels",
-        "Akanji",
-        "Hakimi",
-        "Weigl",
-        "Witsel",
-        "Hazard",
-        "Brandt",
-        "Sancho",
-        "Gotze",
-      ],
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzk",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
     ],
-    score: "4:0",
-    scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-    date: "Nov 9th , 2037",
-    odds: {
-      team1: 1.33,
-      x: 3.25,
-      team2: 6.5,
-    },
-  };
-  // 1
-  for (const[i,player] of game.scored.entries()) {
-    console.log(`goal ${i+1}:${player}`);
-  }
-  for(i = 0;i<game.scored.length;i++){
-    console.log(`goal ${i+1}: ${game.scored[i]}`);
-  }
-  game.scored.forEach(a=>console.log(a));
-  // 2
-  const oddss = Object.values(game.odds)
-  let average = 0;
-  for (const odd of oddss) {
-    average += odd;
-    average /= oddss.length;
-    console.log(average);
-  }
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th , 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// 1
+for (const [i, player] of game.scored.entries()) {
+  console.log(`goal ${i + 1}:${player}`);
+}
+for (i = 0; i < game.scored.length; i++) {
+  console.log(`goal ${i + 1}: ${game.scored[i]}`);
+}
+game.scored.forEach((a) => console.log(a));
+// 2
+const oddss = Object.values(game.odds);
+let average = 0;
+for (const odd of oddss) {
+  average += odd;
+  average /= oddss.length;
+  console.log(average);
+}
+
+// 3
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw " : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+
+
+  
+}
